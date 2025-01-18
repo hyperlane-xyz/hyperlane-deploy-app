85266fdf7657d22171753df43e7184acee7881f7
J M Rossy <jm.rossy@gmail.com>
2025-01-18 10:52:48 -0300

    Restore useRefundDeployerAccounts onSettled param

--- src/features/deployerWallet/refund.ts
+++ src/features/deployerWallet/refund.ts
@@ -19,7 +19,7 @@ import { getTransferTx, sendTxFromWallet } from './transactions';
 import { DeployerWallets } from './types';
 import { useDeployerWallets } from './wallets';
 
-export function useRefundDeployerAccounts() {
+export function useRefundDeployerAccounts(onSettled?: () => void) {
   const multiProvider = useMultiProvider();
   const { chains } = usePastDeploymentChains();
   const { wallets } = useDeployerWallets();
@@ -29,6 +29,7 @@ export function useRefundDeployerAccounts() {
     mutationKey: ['refundDeployerAccounts', chains, accounts],
     mutationFn: () => refundDeployerAccounts(chains, wallets, multiProvider, accounts),
     retry: false,
+    onSettled,
   });
 
   useToastError(
