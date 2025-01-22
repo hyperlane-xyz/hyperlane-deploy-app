9735bce19a5e059e8b598fde65207f36b1d63558
J M Rossy <jm.rossy@gmail.com>
2025-01-22 11:54:29 -0300

    Increase deployment gas units

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,5 +1,6 @@
 export const MIN_CHAIN_BALANCE = 1; // 1 Wei
-// TODO consider reducing this by ~30% if mid-deploying gas top-ups are implemented
-export const WARP_DEPLOY_GAS_UNITS = BigInt(2e7);
+// Should match CLI here:
+// https://github.com/hyperlane-xyz/hyperlane-monorepo/blob/main/typescript/cli/src/consts.ts#L2
+export const WARP_DEPLOY_GAS_UNITS = BigInt(3e7);
 export const REFUND_FEE_PADDING_FACTOR = 1.2;
 export const MIN_DEPLOYER_BALANCE_TO_SHOW = BigInt(1e15); // 0.001 ETH
