3593667885689bfe1b9217b2e5c0a14828e752b2
Xaroz <jasonguo2013jg@gmail.com>
2025-05-16 11:31:10 -0400

    feat: coingeckoId form and modal

---
+++ src/features/deployment/CoinGeckoConfirmationModal.tsx
@@ -0,0 +1,99 @@
+import { ErrorIcon, Modal } from '@hyperlane-xyz/widgets';
+import { Form, Formik, useFormikContext } from 'formik';
+import { SolidButton } from '../../components/buttons/SolidButton';
+import { TextInput } from '../../components/input/TextField';
+import { A } from '../../components/text/A';
+import { H2 } from '../../components/text/Headers';
+import { links } from '../../consts/links';
+import { Color } from '../../styles/Color';
+import { CoinGeckoFormValues } from './warp/types';
+
+const initialValues: CoinGeckoFormValues = {
+  coinGeckoId: '',
+};
+
+export function CoinGeckoConfirmationModal({
+  isOpen,
+  onCancel,
+  onSubmit,
+}: {
+  isOpen: boolean;
+  onCancel: () => void;
+  onSubmit: (values: CoinGeckoFormValues) => void;
+}) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      close={close}
+      panelClassname="p-4 flex flex-col items-center gap-4 max-w-lg"
+    >
+      <H2>Include a coinGeckoId</H2>
+      <p className="text-center text-sm text-gray-700">
+        CoinGecko provides a fundamental analysis of the crypto market. By including one you will be
+        able to track price of collaterized tokens. Below is a suggested coinGeckoId, you can edit
+        this field and check if it is valid by visiting{' '}
+        <A
+          className="underline underline-offset-2 hover:opacity-80 active:opacity-70"
+          href={links.coinGecko}
+        >
+          their site.
+        </A>{' '}
+        If you do not wish to include a coinGeckoId, please press cancel
+      </p>
+      <Formik<CoinGeckoFormValues>
+        initialValues={initialValues}
+        validate={validateForm}
+        onSubmit={onSubmit}
+        validateOnChange={false}
+        validateOnBlur={false}
+      >
+        {() => (
+          <Form className="w-full">
+            <InputSection />
+            <ButtonsSection onCancel={onCancel} />
+          </Form>
+        )}
+      </Formik>
+    </Modal>
+  );
+}
+
+function InputSection() {
+  const { setFieldValue, values, errors } = useFormikContext<CoinGeckoFormValues>();
+
+  return (
+    <>
+      <TextInput
+        value={values.coinGeckoId}
+        onChange={(v) => setFieldValue('coinGeckoId', v)}
+        placeholder="coinGeckoId (ethereum)"
+        className="w-full"
+      />
+      {errors.coinGeckoId && (
+        <div className="flex items-center gap-2 px-3 py-1.5">
+          <ErrorIcon width={14} height={14} color={Color.red['600']} className="shrink-0" />
+          <span className="text-xs text-red-600">{errors.coinGeckoId}</span>
+        </div>
+      )}
+    </>
+  );
+}
+
+function ButtonsSection({ onCancel }: { onCancel: () => void }) {
+  return (
+    <div className="mt-4 flex w-full items-center justify-center gap-12">
+      <SolidButton onClick={onCancel} color="gray" className="min-w-24 px-4 py-2">
+        Cancel
+      </SolidButton>
+      <SolidButton color="primary" className="min-w-24 px-4 py-2" type="submit">
+        Continue
+      </SolidButton>
+    </div>
+  );
+}
+
+function validateForm(values: CoinGeckoFormValues) {
+  if (!values.coinGeckoId) return { coinGeckoId: 'Field is required' };
+
+  return undefined;
+}
--- src/features/deployment/warp/WarpDeploymentSuccess.tsx
+++ src/features/deployment/warp/WarpDeploymentSuccess.tsx
@@ -1,16 +1,18 @@
 import { shortenAddress } from '@hyperlane-xyz/utils';
-import { CopyIcon } from '@hyperlane-xyz/widgets';
+import { CopyIcon, useModal } from '@hyperlane-xyz/widgets';
 import clsx from 'clsx';
 import { RestartButton } from '../../../components/buttons/RestartButton';
 import { A } from '../../../components/text/A';
 import { H1, H2 } from '../../../components/text/Headers';
 import { links } from '../../../consts/links';
 import { Color } from '../../../styles/Color';
+import { CoinGeckoConfirmationModal } from '../CoinGeckoConfirmationModal';
 import { useLatestDeployment, useWarpDeploymentConfig } from '../hooks';
 import { tryCopyConfig } from '../utils';
 
 export function WarpDeploymentSuccess() {
   const { deploymentConfig } = useWarpDeploymentConfig();
+  const { close, isOpen, open } = useModal();
   const firstOwner = Object.values(deploymentConfig?.config || {})[0]?.owner;
   const firstOwnerDisplay = firstOwner ? ` (${shortenAddress(firstOwner)})` : '';
 
@@ -18,51 +20,75 @@ export function WarpDeploymentSuccess() {
   const onClickCopyConfig = () => tryCopyConfig(deploymentContext?.result?.result);
   const onClickCopyDeployConfig = () => tryCopyConfig(deploymentContext?.config.config);
 
-  return (
-    <div className="flex w-full flex-col items-center space-y-4 py-2 text-md">
-      <H1 className="text-center">Your Warp Route is Ready!</H1>
-      <p className="max-w-lg text-center leading-relaxed">
-        You can now use your route to transfer tokens across chains. Remaining funds in the deployer
-        have been refunded.
-      </p>
-      <H2>Next Steps:</H2>
-      <ol className="max-w-sm space-y-4">
-        <li>{`1. Protect your route owner accounts ${firstOwnerDisplay}`}</li>
-        <li>
-          2. Copy and save your{' '}
-          <button
-            className={clsx(styles.link, 'inline-flex items-center gap-1.5')}
-            onClick={onClickCopyDeployConfig}
-          >
-            <span>Warp Deploy config </span>
-            <CopyIcon width={12} height={12} color={Color.black} />
-          </button>{' '}
-          and{' '}
-          <button
-            className={clsx(styles.link, 'inline-flex items-center gap-1.5')}
-            onClick={onClickCopyConfig}
-          >
-            <span> Warp Route config </span>
-            <CopyIcon width={12} height={12} color={Color.black} />
-          </button>
-        </li>
-        <li>
-          3. Add your route to the{' '}
-          <A className={styles.link} href={links.registry}>
-            Hyperlane Registry
-          </A>
-        </li>
-        <li>
-          4.{' '}
-          <A className={styles.link} href={links.warpUiDocs}>
-            Setup a DApp
-          </A>{' '}
-          for users to make transfers
-        </li>
-      </ol>
+  const onCancelCoinGeckoId = () => {
+    close();
+  };
 
-      <RestartButton className="pt-4" />
-    </div>
+  const onConfirmCoinGeckoId = () => [close()];
+
+  return (
+    <>
+      <div className="flex w-full flex-col items-center space-y-4 py-2 text-md">
+        <H1 className="text-center">Your Warp Route is Ready!</H1>
+        <p className="max-w-lg text-center leading-relaxed">
+          You can now use your route to transfer tokens across chains. Remaining funds in the
+          deployer have been refunded.
+        </p>
+        <H2>Next Steps:</H2>
+        <ol className="max-w-sm space-y-4">
+          <li>{`1. Protect your route owner accounts ${firstOwnerDisplay}`}</li>
+          <li>
+            2. Include a{' '}
+            <button
+              className={clsx(styles.link, 'inline-flex items-center gap-1.5')}
+              onClick={open}
+            >
+              <span>coinGeckoId </span>
+              <CopyIcon width={12} height={12} color={Color.black} />
+            </button>{' '}
+            in your deployment config.
+          </li>
+          <li>
+            3. Copy and save your{' '}
+            <button
+              className={clsx(styles.link, 'inline-flex items-center gap-1.5')}
+              onClick={onClickCopyDeployConfig}
+            >
+              <span>Warp Deploy config </span>
+              <CopyIcon width={12} height={12} color={Color.black} />
+            </button>{' '}
+            and{' '}
+            <button
+              className={clsx(styles.link, 'inline-flex items-center gap-1.5')}
+              onClick={onClickCopyConfig}
+            >
+              <span> Warp Route config </span>
+              <CopyIcon width={12} height={12} color={Color.black} />
+            </button>
+          </li>
+          <li>
+            4. Add your route to the{' '}
+            <A className={styles.link} href={links.registry}>
+              Hyperlane Registry
+            </A>
+          </li>
+          <li>
+            5.{' '}
+            <A className={styles.link} href={links.warpUiDocs}>
+              Setup a DApp
+            </A>{' '}
+            for users to make transfers
+          </li>
+        </ol>
+
+        <RestartButton className="pt-4" />
+      </div>
+      <CoinGeckoConfirmationModal
+        isOpen={isOpen}
+        onCancel={onCancelCoinGeckoId}
+        onSubmit={onConfirmCoinGeckoId}
+      />
+    </>
   );
 }
 
