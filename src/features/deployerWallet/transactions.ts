77ea5a752cba0a28bf81517590fed5a3835b68dd
Xaroz <jasonguo2013jg@gmail.com>
2025-05-20 16:56:25 -0400

    chore: include gas price to send transaction

--- src/features/deployerWallet/transactions.ts
+++ src/features/deployerWallet/transactions.ts
@@ -46,12 +46,16 @@ export async function sendTxFromWallet(
   typedTx: TypedTransaction,
   chainName: ChainName,
   multiProvider: MultiProtocolProvider,
+  gasPrice: number | bigint,
 ): Promise<TypedTransactionReceipt> {
   if (typedTx.type === ProviderType.EthersV5 && typedWallet.type === ProviderType.EthersV5) {
     const provider = multiProvider.getEthersV5Provider(chainName);
-    const response = await typedWallet.wallet
-      .connect(provider)
-      .sendTransaction(typedTx.transaction);
+    const connectedWallet = typedWallet.wallet.connect(provider);
+
+    const response = await connectedWallet.sendTransaction({
+      ...typedTx.transaction,
+      gasPrice,
+    });
     const receipt = await response.wait();
     return {
       type: ProviderType.EthersV5,
