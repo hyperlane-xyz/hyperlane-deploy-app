a124545699df4780e1cc1d9bc5ca2c05837a0fd0
Xaroz <jasonguo2013jg@gmail.com>
2025-05-23 10:33:50 -0400

    feat: download deploy/warp config

--- src/features/deployment/utils.ts
+++ src/features/deployment/utils.ts
@@ -9,3 +9,15 @@ export async function tryCopyConfig(config: unknown | undefined) {
   if (result) toast.success('Config copied to clipboard');
   else toast.error('Unable to set clipboard');
 }
+
+export function downloadYamlFile(config: unknown | undefined, filename: string) {
+  if (!config) return;
+  const yamlConfig = stringify(config, { sortMapEntries: true });
+  const blob = new Blob([yamlConfig], { type: 'text/yaml' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = filename;
+  a.click();
+  URL.revokeObjectURL(url);
+}
